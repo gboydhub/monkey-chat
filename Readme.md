1cdcc47981113b981b1c885c77c7b2c796399dc7
gboydhub <eponickstream@gmail.com>
2019-02-20 12:44:18 -0500

    Added readme

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## Monkey Chat
+### Node.js chat application geared for programmers "Code Monkeys"
+
+To use:
+```
+npm init & npm run
+```
+
+Default port is 3000, configured in bin/www
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/:name', function(req, res, next) {
+  console.log(res.params);
   res.render('index', { motd: 'Future site of Monkey Chat' });
 });
 
